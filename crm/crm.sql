0592cdf5e968bda27ea7346450b242190108181b
gupod <gustavo.pires@kellogg.northwestern.edu>
2022-01-18 16:19:54 +0000

    crm practice

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,46 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+drop table contacts;
+drop table activities;
+drop table companies;
+drop table industry;
+drop table salesperson;
+
 -- CREATE TABLES
+
+create table contacts (
+    id integer primary key AUTOINCREMENT,
+    first_name text,
+    last_name text,
+    email_address text,
+    phone_number text,
+    position text,
+    salesperson_id integer,
+    company_id integer
+);
+
+create table activities (
+    id integer primary key AUTOINCREMENT,
+    meeting text,
+    meet_date text,
+    contact_id integer,
+    content text
+);
+
+create table companies (
+    id integer primary key AUTOINCREMENT,
+    company_name text,
+    indsutry_id integer
+);
+
+create table industry (
+    id integer primary key AUTOINCREMENT,
+    industry text
+);
+
+create table salesperson (
+    id integer primary key AUTOINCREMENT,
+    first_name text,
+    last_name text
+);
